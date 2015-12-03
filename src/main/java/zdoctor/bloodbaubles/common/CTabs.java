21fe196762bd86e64e5060382327a79ba56c90c1
Z-Doctor <seedofdiscovery@gmail.com>
2015-12-02 23:52:14 -0800

    Fixed Bug
    Creative tab named wrong

--- src/main/java/zdoctor/bloodbaubles/common/CTabs.java
+++ src/main/java/zdoctor/bloodbaubles/common/CTabs.java
@@ -5,9 +5,9 @@ import net.minecraft.item.Item;
 
 public class CTabs {
   public static void preInit() {};
-  
-  public static CreativeTabs BloodRings = new CreativeTabs("MagicRings") {
-    
+
+  public static CreativeTabs BloodRings = new CreativeTabs("BloodBaubles") {
+
     @Override
     public Item getTabIconItem() {
       return ZRings.RingGoldMasterRing;
